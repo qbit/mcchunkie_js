73125023a49c294772abeb51dced0436ac29f651
Aaron Bieber <qbit@qbit.io>
2012-06-09 10:15:38 -0700

    added some tmnt!

---
+++ plugins/tmnt.js
@@ -0,0 +1,18 @@
+(function( botname, to, from, msg, store, cb ) {
+	'use strict';
+  var resp, phrases = [
+    "Yee haw! Ninja cowboy!",
+    "A true Ninja is a master of himself and his environment, so don't forget: We're turtles!",
+    "The rat is the cleanest one.",
+    "Amazing, guys, and I thought all the really good dungeons were in Europe.",
+    "Yeah, remind me to drop a line to Ralph Nader!",
+    "Boy, whatever happened to 'service with a smile'?",
+    "SHREDDER!"
+  ];
+
+  if ( msg.indexOf( botname ) > -1 && msg.indexOf( 'tmnt' ) ) {
+    resp = phrases[ Math.floor( Math.random() * phrases.length ) ];
+  }
+
+  cb.call( null, to, from, resp );
+});
