0089cf200b5e4f8a03b0a5bcf055c9d47bcb5257
Aaron Bieber <qbit@qbit.io>
2012-06-09 09:39:04 -0700

    remove console.log

--- plugins/reverse.js
+++ plugins/reverse.js
@@ -1,6 +1,5 @@
 (function( botname, to, from, msg, storage, cb ) {
   // Plugin to reverse every msg that is passed in.
-  console.log( msg.indexOf( botname ), msg.indexOf( 'reverse' ));
   if ( msg.indexOf( botname ) > -1 && msg.indexOf( 'reverse' ) > -1 ) {
     msg = msg.replace( botname, '' );
     msg = msg.replace( ':', '' );
