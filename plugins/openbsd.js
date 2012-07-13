c7233849f5ae9e6eaa3bd1983750de58e46abcb5
Aaron Bieber <qbit@qbit.io>
2012-07-12 22:16:04 -0700

    openbsd displaying logic working - need to make pubsub notification stuff

--- plugins/openbsd.js
+++ plugins/openbsd.js
@@ -1,6 +1,11 @@
 (function( helper, to, from, msg, store, sh_store, cb ) {
 	'use strict';
-  var resp = '', parts, list, p;
+
+  String.prototype.ucFirst = function() {
+    return this.charAt(0).toUpperCase() + this.substr(1);
+  };
+
+  var resp = [], parts, list, p, cat, scat, s, a,b,c,d;
 
   if ( helper.isRelevant( msg ) ) {
     if ( msg.match( /openbsd/i ) ) {
@@ -12,13 +17,44 @@
 
       console.log( 'Parts: ', parts );
 
+      cat = parts[1];
+      scat = parts[2];
+
       // resp = ':( - hurry qbit! fix it!';
-      if ( store[ parts[0] ] || sh_store[ parts[0] ] ) {
-        p = parts[0];
-        list = Object.keys( sh_store[ p ] ); 
-        console.log( list );
+      s = store.openbsd || sh_store.openbsd;
+
+      console.log( 'cat: %s, scat: %s', cat, scat );
+
+      if ( s[cat] ) {
+        // showing packages or sets for all
+        for ( a in s[cat] ) {
+          if ( s[cat][a] ) {
+            console.log( 1, a, cat, s[cat][a] );
+            resp.push( a, ':', s[cat][a].date + '.' );
+          }
+        }
+      } else {
+        if ( s[scat] ) {
+          if ( s[scat][cat] ) {
+            for ( a in s[scat][cat] ) {
+              console.log( 2, a, cat, s[scat][cat][a] );
+              resp.push( cat, ':', s[scat][cat][a] + '.' );
+            }
+          }
+        } else {
+          for ( a in s ) {
+            for( b in s[a] ) {
+              // if ( b === cat ) {
+                // console.log( b, a, s[a][b] );
+                // resp.push( b, a, '=>',  s[a][b].date + '.' );
+              // } else {
+                resp.push( b, a, '=>',  s[a][b].date + '. \n' );
+              // }
+            }
+          }
+        }
       }
     }
   }
-  cb.call( null, to, from, resp );
+  cb.call( null, to, from, resp.join( ' ' ).toString() );
 });
--- plugins/tmnt.js
+++ plugins/tmnt.js
@@ -4,9 +4,12 @@
     "Yee haw! Ninja cowboy!",
     "A true Ninja is a master of himself and his environment, so don't forget: We're turtles!",
     "The rat is the cleanest one.",
-    "Amazing, guys, and I thought all the really good dungeons were in Europe.",
     "Yeah, remind me to drop a line to Ralph Nader!",
     "Boy, whatever happened to 'service with a smile'?",
+    "TECHNODROME!!",
+    "Hardcore crazy!",
+    "Yeah, pizza.",
+    "Yeah! Turtles!",
     "SHREDDER!"
   ];
 
