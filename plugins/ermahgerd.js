0fe59cdc76ee1eed530822afb8b5abc35b7ffcf3
Aaron Bieber <qbit@qbit.io>
2012-08-08 06:52:33 -0700

    slight change to ermaggerd

--- plugins/ermahgerd.js
+++ plugins/ermahgerd.js
@@ -2,29 +2,40 @@
   'use strict';
   var resp;
 
-  if ( helper.isRelevant( msg ) ) {
-    if ( msg == helper.botname + ": toggle e" ) {
-      store.doit = !store.doit;
+  if ( ! store.ermg ) {
+    store.ermg = function( s ) {
+      s = s.toUpperCase();
+      s = s.replace(/[AEIOUY]r(?! )/g, 'E');
+      s = s.replace(/AA/g, 'A');
+      s = s.replace(/EE/g, 'E');
+      s = s.replace(/II/g, 'I');
+      s = s.replace(/OO/g, 'O');
+      s = s.replace(/UU/g, 'U');
+      s = s.replace(/YY/g, 'Y');
+      s = s.replace(/[AEIOUY]{2,}/g, 'E');
+      s = s.replace(/[AEIOUY](?! )/g, 'ER');
+      s = s.replace(/[Y]/g, 'ER');
+      s = s.replace(/ERH/g, 'ER');
+      s = s.replace(/ERR/g, 'ER');
+      s = s.replace(/MER/g, 'MAH');
+
+      return s;
     }
   }
 
-  msg = msg.toUpperCase();
-  msg = msg.replace(/[AEIOUY]r(?! )/g, 'E');
-  msg = msg.replace(/AA/g, 'A');
-  msg = msg.replace(/EE/g, 'E');
-  msg = msg.replace(/II/g, 'I');
-  msg = msg.replace(/OO/g, 'O');
-  msg = msg.replace(/UU/g, 'U');
-  msg = msg.replace(/YY/g, 'Y');
-  msg = msg.replace(/[AEIOUY]{2,}/g, 'E');
-  msg = msg.replace(/[AEIOUY](?! )/g, 'ER');
-  msg = msg.replace(/[Y]/g, 'ER');
-  msg = msg.replace(/ERH/g, 'ER');
-  msg = msg.replace(/ERR/g, 'ER');
-  msg = msg.replace(/MER/g, 'MAH');
+  if ( helper.isRelevant( msg ) ) {
+    if ( msg == helper.botname + ": toggle e" ) {
+      store.doit = !store.doit;
+      if ( store.doit ) {
+        resp = store.ermg( 'doing it!' );
+      }
+      cb.call( null, to, from, resp );
+      return;
+    }
+  }
 
   if ( store.doit ) {
-    resp = msg;
+    resp = store.ermg( msg );
   }
 
   cb.call( null, to, from, resp );
