d9949d78eb2aa5e902829c80acb640049b3a6a7a
Aaron Bieber <qbit@qbit.io>
2012-08-08 06:53:13 -0700

    show OpenBSD vs openbsd

--- plugins/version.js
+++ plugins/version.js
@@ -1,9 +1,16 @@
 (function( helper, to, from, msg, store, sh_store, cb ) {
   'use strict';
   var resp;
+
+  if ( ! 'asdf'.ucFirst ) {
+    String.prototype.ucFirst = function() {
+      return this.charAt(0).toUpperCase() + this.substr(1);
+    };
+  }
+
   if ( helper.isRelevant( msg ) ) {
     if ( msg.match( 'version' ) ) {
-      resp = "I am running on Node.JS " + process.version + ' on ' + process.platform + '.';
+      resp = "I am running on Node.JS " + process.version + ' on ' + process.platform.ucFirst().replace( 'bsd', 'BSD' ) + '.';
     }
   }
   cb.call( null, to, from, resp );
