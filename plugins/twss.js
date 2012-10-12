146facf98b154da3c8d8e90b67037e58d565d4ed
Aaron Bieber <qbit@qbit.io>
2012-10-12 08:01:54 -0600

    remember what was twssed .. and no on that

--- plugins/twss.js
+++ plugins/twss.js
@@ -19,6 +19,7 @@
 
   if ( ! store.msgs ) {
     store.msgs = [];
+    store.spoken_twsses = [];
   }
 
   store.msgs.push( msg );
@@ -27,6 +28,10 @@
 
     if ( store.msgs.length >  10 ) {
       store.msgs.shift();
+    }
+
+    if ( store.spoken_twsses.length >  10 ) {
+      store.spoken_twsses.shift();
     }
 
     if ( ! store.bays ) {
@@ -51,12 +56,15 @@
       //   console.log( 'prev msg: ' + store.msgs[1] );
         if ( msg.match( /^twss$/i ) || msg === helper.botname + ': yes' ) {
           store.bays.train( store.msgs[ store.msgs.length - 2 ], 'funny', function() {
+            store.spoken_twsses.push( store.msgs[ store.msgs.length - 2 ] );
             resp = 'Added funny: "' + store.msgs[ store.msgs.length - 2 ] + '"';
             cb.call( null, to, from, resp );
           });
         } if ( msg === helper.botname + ': no' ) {
-          store.bays.train( store.msgs[ store.msgs.length - 2 ], 'notfunny', function() {
-            resp = 'Sorry: "' + store.msgs[ store.msgs.length - 2 ] + '"';
+          // store.bays.train( store.msgs[ store.msgs.length - 2 ], 'notfunny', function() {
+          store.bays.train( store.spoken_twsses[ store.spoken_twsses.length - 1 ], 'notfunny', function() {
+            // resp = 'Sorry: "' + store.msgs[ store.msgs.length - 2 ] + '"';
+            resp = 'Sorry: "' + store.spoken_twsses[ store.spoken_twsses.length - 1 ] + '"';
             cb.call( null, to, from, resp );
           });
         } if ( msg.match( /^twss\?$/i ) ) {
@@ -66,6 +74,7 @@
           }
         } else {
           if ( cat === 'funny' ) {
+            store.spoken_twsses.push( msg );
             resp = responses[ helper.rand( responses.length ) ];
             cb.call( null, to, from, resp );
           }
@@ -76,4 +85,5 @@
       console.log( e );
     }
   }
+
 });
