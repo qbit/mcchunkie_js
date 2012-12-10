2dd8a3cd4884a9e514ae0e5f122a1bc223208b30
Aaron Bieber <deftly@gmail.com>
2012-12-10 07:40:58 -0700

    oyfb

---
+++ plugins/oyfb.js
@@ -0,0 +1,18 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp, 
+  resps = [
+    'reporting for dootie!',
+    '? = 33',
+    '28c',
+    '1003f',
+    'probably potato',
+    '....',
+    'um.'
+    ];
+  if ( msg.match( /@oneofyoufuckingbots/ ) ) {
+    resp = resps[ helper.rand( resps.length ) ];
+  }
+
+  cb.call( null, to, from, resp );
+});
