948c3910513851cc51602bffab519d1c95a5329d
Aaron Bieber <deftly@gmail.com>
2012-12-10 07:41:10 -0700

    putitback

---
+++ plugins/putitback.js
@@ -0,0 +1,12 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp;
+
+  if ( msg.match( /@tableflip/ ) ) {
+    resp = '┬──┬﻿ ¯\_(ツ)';
+  }
+
+  setTimeout( function() {
+    cb.call( null, to, from, resp );
+  }, Math.ceil( Math.random( ) * 10000 ) );
+});
