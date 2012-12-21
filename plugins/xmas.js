4faf6934c29efa749d7be3b384fa94c4fc6f0454
Aaron Bieber <deftly@gmail.com>
2012-12-21 12:28:03 -0700

    added xmas

---
+++ plugins/xmas.js
@@ -0,0 +1,23 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp;
+  // if ( helper.isRelevant( msg ) ) {
+  // }
+
+  // if ( msg === helper.botname + ': help' ) {
+  // }
+  if ( msg.match( /merry christmas/i ) ) {
+    resp = [
+      "˛ °.★__ *★* *˛.",
+      "˛ °_██_*。*./ \ .˛* .˛.*.★* *★ 。*",
+      "˛. (´• ̮•)*˛°*/.♫.♫\*˛.* ˛_Π_____. * ˛*",
+      ".°( . • . ) ˛°./• '♫ ' •\.˛*./______/~＼ *. ˛*.。˛* ˛. *。",
+      "*(...'•'.. ) *˛╬╬╬╬╬˛°.｜田田 ｜門｜╬╬╬╬ .",
+      "... ... ¯˜\"*°•♥•°*\"˜¯`´¯˜\"*°•♥•°*\"˜¯` ´¯˜\"*°´¯˜\"*°•♥•°*\"˜¯`´¯˜\"*°•"
+    ];
+
+    resp = resp.join( '\n' );
+  }
+
+  cb.call( null, to, from, resp );
+});
