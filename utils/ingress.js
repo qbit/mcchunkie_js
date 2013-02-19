ebe9e7a4940c730945ae24652b74aa0e01b25d87
Aaron Bieber <deftly@gmail.com>
2013-02-18 17:18:34 -0700

    ingress portal info

---
+++ utils/ingress.js
@@ -0,0 +1,17 @@
+var reds = require( 'reds'),
+fs = require( 'fs' ),
+url = require( 'url' ),
+search = reds.createSearch( 'pueblo_ingress' ), i, l, data = [];
+
+fs.readFile( 'pueblo_ingress.js', function( err, data ) {
+  data = JSON.parse( data );
+  for ( i = 0, l = data.length; i < l; i++ ) {
+    if ( i < 5 ) {
+      console.log( data[i] );
+    }
+
+    // search.index( data[i].Name, JSON.stringify( data[i].geometry ) );
+    search.index( data[i].properties.Name, JSON.stringify( data[i].geometry ) );
+  }
+  process.exit(0);
+});
