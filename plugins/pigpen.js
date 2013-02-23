971886932e7c0344cf64467a1a7a6796a3fead5f
Aaron Bieber <deftly@gmail.com>
2013-02-22 17:22:39 -0700

    pigpen

---
+++ plugins/pigpen.js
@@ -0,0 +1,26 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp, i;
+
+  if ( ! store.chars ) {
+    store.chars = { 
+      b: '␣',
+      c: '∟',
+      e: '☐',
+      n: '☒'
+    };
+  }
+
+  if ( msg.match( /^pigpen: / ) ) {
+    msg = msg.replace( /^pigpen: /, '' );
+    for ( i in store.chars ) {
+      if ( store.chars.hasOwnProperty( i ) ) {
+        msg = msg.replace( new RegExp(i, "g"), store.chars[i] );
+      }
+    }
+    resp = msg;
+    console.log( msg );
+  }
+
+  cb.call( null, to, from, resp );
+});
