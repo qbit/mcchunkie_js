0efe3696d68c47c3f93d4ded34683c283a28e690
Aaron Bieber <deftly@gmail.com>
2013-05-15 07:41:23 -0600

    freedom!

---
+++ plugins/free.js
@@ -0,0 +1,12 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp;
+  if ( helper.isRelevant( msg ) ) {
+  }
+
+  if ( msg === 'freedom' ) {
+    resp = 'MERIKA!';
+  }
+
+  cb.call( null, to, from, resp );
+});
