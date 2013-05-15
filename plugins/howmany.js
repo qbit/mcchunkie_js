2c90c08d40afd5ccc2bd05ae9c8c3c1585580190
Aaron Bieber <deftly@gmail.com>
2013-05-15 07:40:50 -0600

    disable ports, add howmany

--- plugins/howmany.js
+++ plugins/howmany.js
@@ -6,10 +6,21 @@
   if ( ! store.db ) {
     helper.sqlite.verbose();
     store.db = new helper.sqlite.Database( '/usr/local/share/sqlports' );
-    store.query = "select count(*) as num from ports where maintainer like ?";
+    store.query = "select count(distinct pkgname) as num from ports where maintainer like ?";
+    store.query_most = "select maintainer as maintainer, count(*) as num from ports where maintainer <> 'The OpenBSD ports mailing-list <ports@openbsd.org>' group by maintainer order by count(*) desc limit 1";
   }
 
   var a, p;
+  if ( msg.match( /^mostports:$/ ) ) {
+    p = store.db.prepare( store.query_most );
+
+    p.get( [ ], function( err, row ) {
+      if ( !err ) {
+        console.log( row );
+        cb.call( null, to, from, row.maintainer + ' has the most ports (' + row.num + ').' );
+      }
+    });
+  }
   if ( msg.match( /^howmany: / ) ) {
     msg = msg.replace( /^howmany: /, '' );
     p = store.db.prepare( store.query );
