8614a073e6235de8a89e2e0f4958c3d5b7621042
Aaron Bieber <deftly@gmail.com>
2013-08-26 17:07:10 -0600

    protip

---
+++ plugins/protip.js
@@ -0,0 +1,19 @@
+(function(helper, to, from, msg, store, sh_store, cb) {
+  'use strict';
+  var resp;
+
+  if (!store.rclient) {
+	  store.rclient = redis.createClient();
+  }
+
+  if (msg.match(/^brotip\?|^protip\?|^pro-tip\?/i)) {
+	  store.rclient.srandmember('protip', function(e, d) {
+		  cb.call(null, to, from, d);
+	  });
+  }
+
+  if (msg.match(/^brotip:|^protip:|^pro-tip:/i )) {
+	  msg = msg.replace(/^brotip:|^protip:|^pro-tip:/i, '');
+	  store.rclient.sadd('protip', msg);
+  }
+});
