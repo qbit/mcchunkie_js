f4d60448ec6cce1d7959cd67838cd13fe6dab1dc
Aaron Bieber <deftly@gmail.com>
2013-08-29 20:32:00 -0600

    reset user values after max

--- plugins/ham_tests.js
+++ plugins/ham_tests.js
@@ -87,6 +87,8 @@
 					  if ( store[id].takers[frm] && store[id].takers[frm].total ) {
 						  if ( store[id].takers[frm].total === 30 ) {
 							  cb.call(null, t, frm, frm + ': ' + store.calcRes( frm, store[id].takers[frm] ));
+							  store[id].takers[frm].total = 0;
+							  store[id].takers[frm].count = 0;
 						  }
 					  }
 				  }
