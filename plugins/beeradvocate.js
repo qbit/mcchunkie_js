816833ad44620dc184f6ae19878be1d45f13a929
Aaron Bieber <deftly@gmail.com>
2014-03-17 17:43:30 -0600

    beer advocate plugin

---
+++ plugins/beeradvocate.js
@@ -0,0 +1,48 @@
+(function( helper, to, from, msg, store, sh_store, cb ) {
+  'use strict';
+  var resp;
+
+  if (! store.ba) {
+	  store.ba = require('beer-advocate-api');
+  }
+
+  function pretty(rev) {
+	  var map = {
+		  beer_name: 'Name',
+		  beer_style: 'Style',
+		  beer_abv: 'ABV',
+		  brewery_name: 'Brewery',
+		  brewery_state: 'State',
+		  brewery_country: 'Country',
+		  ba_score: 'BA Score',
+		  ba_rating: 'BA Rating',
+		  bros_score: 'Bros Score',
+		  rAvg: 'rAvg',
+		  pDev: 'pDev'
+	  }, i, o = [];
+
+	  if (rev) {
+		  for(i in rev) {
+			  if (map.hasOwnProperty(i) && rev.hasOwnProperty(i)) {
+				  o.push(map[i] + ': ' + rev[i] || '?');
+			  }
+		  }
+	  }
+
+	  o = o.sort();
+
+	  return(o.join(', '));
+  }
+
+  if (msg.match(/^ba:/)) {
+	  msg = msg.replace(/^ba:/, '');
+	  store.ba.beerSearch(msg, function(beers) {
+		  store.ba.beerPage(beers[0].beer_url, function(reviews) {
+			  if ( reviews.length >= 1) {
+				  cb.call( null, to, from, pretty(reviews[0]));
+			  }
+		  });
+      	  });
+
+  }
+});
