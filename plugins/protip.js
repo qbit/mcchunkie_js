4355e899a422d578a80aaa612c25fe766d61aa1a
Aaron Bieber <deftly@gmail.com>
2014-03-24 16:24:02 -0600

    auth for redis

--- plugins/protip.js
+++ plugins/protip.js
@@ -5,16 +5,29 @@
 
   if (!store.rclient) {
 	  store.rclient = redis.createClient();
+	  store.authed = false;
+	  if (process.env.REDIS_PASS) {
+		  store.rclient.auth(process.env.REDIS_PASS, function() {
+			  store.authed = true;
+			  console.log('protip authed');
+		  });
+	  } else {
+		  store.authed = true;
+	  }
   }
 
   if (msg.match(/^brotip\?|^protip\?|^pro-tip\?/i)) {
-	  store.rclient.srandmember('protip', function(e, d) {
-		  cb.call(null, to, from, d);
-	  });
+	  if (store.authed) {
+		  store.rclient.srandmember('protip', function(e, d) {
+			  cb.call(null, to, from, d);
+		  });
+	  }
   }
 
   if (msg.match(/^brotip:|^protip:|^pro-tip:/i )) {
-	  msg = msg.replace(/^brotip: |^protip: |^pro-tip: /i, '');
-	  store.rclient.sadd('protip', msg);
+	  if (store.authed) {
+		  msg = msg.replace(/^brotip: |^protip: |^pro-tip: /i, '');
+		  store.rclient.sadd('protip', msg);
+	  }
   }
 });
