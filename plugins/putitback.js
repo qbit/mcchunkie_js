dd91c99335e0e2a3090313bc3c101fae76bc6a39
Aaron Bieber <aaron@bolddaemon.com>
2015-06-25 12:17:01 -0600

    put it back

--- plugins/putitback.js
+++ plugins/putitback.js
@@ -1,5 +1,5 @@
 // Desc: once a table has been fliped, politely put it back
-(function( helper, to, from, msg, store, sh_store, cb ) {
+(function( helper, to, from, msg, store, sh_store, cb, proto ) {
   'use strict';
   var resp;
 
@@ -8,6 +8,6 @@
   }
 
   setTimeout( function() {
-    cb.call( null, to, from, resp );
+    cb.call( null, to, from, resp, proto );
   }, Math.ceil( Math.random( ) * 10000 ) );
 });
