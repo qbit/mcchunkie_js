ee51f5f0d5a10dfc056c15c53308df5289e5a2aa
Aaron Bieber <aaron@bolddaemon.com>
2015-06-25 15:00:59 -0600

    wb

--- plugins/wb.js
+++ plugins/wb.js
@@ -1,5 +1,5 @@
 // Desc: respond to welcom backs
-(function( helper, to, from, msg, store, sh_store, cb ) {
+(function( helper, to, from, msg, store, sh_store, cb, proto ) {
 	'use strict';
   var resp;
   if ( helper.isRelevant( msg ) ) {
@@ -9,5 +9,5 @@
     }
   }
 
-  cb.call( null, to, from, resp );
+  cb.call( null, to, from, resp, proto );
 });
