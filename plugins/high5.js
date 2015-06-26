1b124b004a3866b49d7e6500ea993cbe1b6239d2
Aaron Bieber <aaron@bolddaemon.com>
2015-06-26 10:41:15 -0600

    high5

--- plugins/high5.js
+++ plugins/high5.js
@@ -1,13 +1,22 @@
 // Desc: respond to high5's
 (function( helper, to, from, msg, store, sh_store, cb, proto ) {
-	'use strict';
+  'use strict';
   var resp;
+
+  if (msg.match(/^\/help$|^help:$/)) {
+		resp = 'high5 - look for high5s in messages to the bot.. respond to them!';
+
+                cb.call(null, to, from, resp, proto);
+                return;
+  }
+
+
   if ( helper.isRelevant( msg ) ) { 
     msg = msg.trim();
-    if ( msg === helper.botname + ': o/'  || msg === 'o/ ' + helper.botname ) {
+    if (msg.match(/o\/)) {
       resp = '\\o';
     }
-    if ( msg === helper.botname + ': \\o'  || msg === '\\o ' + helper.botname ) {
+    if (msg.match(/\\o/)) {
       resp = 'o/';
     }
   }
