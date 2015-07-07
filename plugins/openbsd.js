26d0bb5a206741a34f8327015a189bf9f2df4ea9
Aaron Bieber <aaron@bolddaemon.com>
2015-07-07 16:57:29 -0600

    Update OpenBSD plugin

--- plugins/openbsd.js
+++ plugins/openbsd.js
@@ -1,53 +1,54 @@
 // Desc: uses the pubsub / storage features and an external app ( openbsd_mon to keep track of openbsd snapshot releases.
-(function( helper, to, from, msg, store, sh_store, cb ) {
-	'use strict';
+(function( helper, to, from, msg, store, sh_store, cb, proto ) {
+    'use strict';
 
-  String.prototype.ucFirst = function() {
-    return this.charAt(0).toUpperCase() + this.substr(1);
-  };
+    String.prototype.ucFirst = function() {
+	return this.charAt(0).toUpperCase() + this.substr(1);
+    };
 
-  var resp = [], parts, list, p, cat, scat, s, a,b,c,d;
+    var resp = [], parts, list, p, cat, scat, s, a,b,c,d;
 
-  if ( helper.isRelevant( msg ) ) {
-    if ( msg.match( /openbsd/i ) ) {
-      msg = msg.replace( helper.botname, '' );
-      msg = msg.replace( ':', '' );
-      msg = msg.trim();
+    if ( helper.isRelevant( msg ) ) {
+	if ( msg.match( /^openbsd|^\/openbsd/i ) ) {
+	    msg = msg.replace( helper.botname, '' );
+	    msg = msg.replace( ':', '' );
+	    msg = msg.replace( '^/', '' );
+	    msg = msg.trim();
 
-      parts = msg.split( ' ' ); 
+	    parts = msg.split( ' ' ); 
 
-      cat = parts[1];
-      scat = parts[2];
+	    cat = parts[1];
+	    scat = parts[2];
 
-      s = store.openbsd || sh_store.openbsd;
+	    s = store.openbsd || sh_store.openbsd;
 
-      if ( s[cat] && ! scat ) {
-        for ( a in s[cat] ) {
-          resp.push( a, '=>', s[cat][a].date + '.' );
-        }
-      } else if( s[cat] && scat ) {
-        resp.push( scat, '=>', s[cat][scat].date + '.' );
-      } else {
-        if ( ! cat ) {
-          for ( cat in s ) {
-            for ( a in s[cat] ) {
-                resp.push( cat, a, '=>', s[cat][a].date + '.' );
-            }
-            resp.push( '\n' );
-          }
-        } else {
-          for ( c in s ) {
-            if ( c.length === cat.length ) {
-              resp.push( "did you mean '" + c + "'?" );
-            }
-          }
-          if ( resp.length === 0 ) {
-            resp.push( 'not sure what that is..' );
-          }
-        }
-      }
+	    if ( s[cat] && ! scat ) {
+		for ( a in s[cat] ) {
+		    resp.push( a, '=>', s[cat][a].date + '.' );
+		}
+	    } else if( s[cat] && scat ) {
+		resp.push( scat, '=>', s[cat][scat].date + '.' );
+	    } else {
+		if ( ! cat ) {
+		    for ( cat in s ) {
+			for ( a in s[cat] ) {
+			    resp.push( cat, a, '=>', s[cat][a].date + '.' );
+			}
+			resp.push( '\n' );
+		    }
+		} else {
+		    for ( c in s ) {
+			if ( c.length === cat.length ) {
+			    resp.push( "did you mean '" + c + "'?" );
+			}
+		    }
+		    if ( resp.length === 0 ) {
+			resp.push( 'not sure what that is..' );
+		    }
+		}
+	    }
+	}
     }
-  }
 
-  cb.call( null, to, from, resp.join( ' ' ).toString() );
+    cb.call( null, to, from, resp.join( ' ' ).toString(), proto );
 });
