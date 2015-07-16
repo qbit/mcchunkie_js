0cb0c37f9f8965ea466af058ae119f8af2fd1f25
Aaron Bieber <aaron@bolddaemon.com>
2015-07-16 10:23:36 -0600

    pass proto

--- plugins/emoji.js
+++ plugins/emoji.js
@@ -1,4 +1,4 @@
-(function( helper, to, from, msg, store, sh_store, cb ) {
+(function( helper, to, from, msg, store, sh_store, cb, proto ) {
     'use strict';
     var resp;
 
@@ -4410,6 +4410,6 @@ SOFTWARE.
 	}
 
 	resp = r.join(' ');
-	cb.call( null, to, from, resp );
+	cb.call( null, to, from, resp, proto );
     }
 });
