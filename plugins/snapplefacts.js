922e7c82d7dc93342f3acf83ebe935bcf0db9475
Aaron Bieber <aaron@bolddaemon.com>
2015-08-12 16:47:47 -0600

    snapplefacts

---
+++ plugins/snapplefacts.js
@@ -0,0 +1,26 @@
+// Desc: return random snapplefacts
+(function(helper, to, from, msg, store, sh_store, cb, proto) {
+  'use strict';
+  var resp;
+
+  if (!store.rclient) {
+	  store.rclient = redis.createClient();
+	  store.authed = false;
+	  if (process.env.REDIS_PASS) {
+		  store.rclient.auth(process.env.REDIS_PASS, function() {
+			  store.authed = true;
+			  console.log('snapplefact authed');
+		  });
+	  } else {
+		  store.authed = true;
+	  }
+  }
+
+  if (msg.match(/^snapplefact\?/i)) {
+	  if (store.authed) {
+		  store.rclient.srandmember('snapplefact', function(e, d) {
+			  cb.call(null, to, from, d, proto);
+		  });
+	  }
+  }
+});
