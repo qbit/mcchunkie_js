11d4107be3b71c00a9cce71c5dfe2a42082f399c
Aaron Bieber <aaron@bolddaemon.com>
2015-08-24 10:24:46 -0600

    add a counter to only respond after a few buellers

--- plugins/bueller.js
+++ plugins/bueller.js
@@ -1,11 +1,17 @@
 // Desc: Bueller!!
 (function( helper, to, from, msg, store, sh_store, cb, proto ) {
   'use strict';
-  var resp;
+    var resp;
 
-  if ( msg.match( /bueller/i ) ) {
-    resp = "I'm sorry I can't come to the door right now. I'm very ill and I'm afraid that in my weekend condition, I could take a nasty spill down the stairs and subject myself to further school absences.";
-  }
+    if (!store.count) {
+	store.count = 0;
+    }
 
-  cb.call( null, to, from, resp, proto );
+    if ( msg.match( /bueller/i ) ) {
+	store.count++;
+	if (store.count >= 4) {
+	    resp = "I'm sorry I can't come to the door right now. I'm very ill and I'm afraid that in my weekend condition, I could take a nasty spill down the stairs and subject myself to further school absences.";
+	}
+    }
+    cb.call( null, to, from, resp, proto );
 });
