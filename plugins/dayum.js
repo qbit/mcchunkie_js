df838fcf2452919e0e4e02e5e56fc2dc0f6d560d
Aaron Bieber <aaron@bolddaemon.com>
2015-12-04 19:15:55 -0700

    indent

--- plugins/dayum.js
+++ plugins/dayum.js
@@ -1,25 +1,25 @@
 // Desc: Daaayum Daaayyyyuuummm DAAAAAAYYYYUUUUMMMMMMMM
 (function( helper, to, from, msg, store, sh_store, cb, proto ) {
-  'use strict';
-  var resp;
+    'use strict';
+    var resp;
 
-  if (msg.match(/^\/help$|^help:$/)) {
-		if (proto === 'telegram') {
-			resp = '/beer [beer] - get BeerAdvocate information for [beer].';
-		} else {
-			resp = 'beer: [beer] - get BeerAdvocate information for [beer].';
-		}
+    if (msg.match(/^\/help$|^help:$/)) {
+	if (proto === 'telegram') {
+	    resp = '/beer [beer] - get BeerAdvocate information for [beer].';
+	} else {
+	    resp = 'beer: [beer] - get BeerAdvocate information for [beer].';
+	}
 
-                cb.call(null, to, from, resp, proto);
-                return;
-  }
+	cb.call(null, to, from, resp, proto);
+	return;
+    }
 
-  if ( msg.match( /cheese burger/i ) ) {
-    resp = "Dayum DAYum DAAAAAYUUUUUMMMMM!!!!";
-  }
-  if ( msg.match( /5 guys/i ) ) {
-    resp = "Dayum DAYum DAAAAAYUUUUUMMMMM!!!!";
-  }
+    if ( msg.match( /cheese burger/i ) ) {
+	resp = "Dayum DAYum DAAAAAYUUUUUMMMMM!!!!";
+    }
+    if ( msg.match( /5 guys/i ) ) {
+	resp = "Dayum DAYum DAAAAAYUUUUUMMMMM!!!!";
+    }
 
-  cb.call( null, to, from, resp, proto );
+    cb.call( null, to, from, resp, proto );
 });
