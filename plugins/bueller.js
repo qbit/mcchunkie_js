08797af4b855aba06438f5fa36717e8c9aaa401a
Aaron Bieber <aaron@bolddaemon.com>
2015-12-04 19:23:33 -0700

    fix spelling

--- plugins/bueller.js
+++ plugins/bueller.js
@@ -10,7 +10,7 @@
     if ( msg.match( /bueller/i ) ) {
 	store.count++;
 	if (store.count >= 4) {
-	    resp = "I'm sorry I can't come to the door right now. I'm very ill and I'm afraid that in my weekend condition, I could take a nasty spill down the stairs and subject myself to further school absences.";
+	    resp = "I'm sorry I can't come to the door right now. I'm very ill and I'm afraid that in my weakened condition, I could take a nasty spill down the stairs and subject myself to further school absences.";
 	}
     }
     cb.call( null, to, from, resp, proto );
