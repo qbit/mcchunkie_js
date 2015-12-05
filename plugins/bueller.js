16c2a426bdbe7c5a9e37a756ea4244c11cbdca5c
Aaron Bieber <aaron@bolddaemon.com>
2015-12-04 19:26:16 -0700

    reset counter and only do it on > 4

--- plugins/bueller.js
+++ plugins/bueller.js
@@ -9,8 +9,9 @@
 
     if ( msg.match( /bueller/i ) ) {
 	store.count++;
-	if (store.count >= 4) {
+	if (store.count > 4) {
 	    resp = "I'm sorry I can't come to the door right now. I'm very ill and I'm afraid that in my weakened condition, I could take a nasty spill down the stairs and subject myself to further school absences.";
+	    store.count = 0;
 	}
     }
     cb.call( null, to, from, resp, proto );
