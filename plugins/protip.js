188d4bd71cd6ec423d70c21389d84b16733e1e37
Aaron Bieber <aaron@bolddaemon.com>
2016-03-30 18:40:30 -0600

    include vote counts in tips

--- plugins/protip.js
+++ plugins/protip.js
@@ -17,17 +17,23 @@
   }
 
   if (msg.match(/^\/[pb]rotip\?|^brotip\?|^protip\?|^pro-tip\?/i)) {
-	  if (store.authed) {
-		  store.rclient.srandmember('protip', function(e, d) {
-			  cb.call(null, to, from, d, proto);
+      if (store.authed) {
+	  store.rclient.llrange('l_protips', function(e, max) {
+	      var idx = helper.rand(parseInt(Math.ceil(max), 10));
+	      store.rclient.lindex(idx, 'l_protips', function(e, d) {
+		  store.rclient.hget('protip_votes', idx, function(e, count) {
+		      d = "(" + count + ") " + d;
+		      cb.call(null, to, from, d, proto);
 		  });
-	  }
+	      });
+	  });
+      }
   }
 
-  if (msg.match(/^\/[pb]rotip:|^brotip:|^protip:|^pro-tip:/i )) {
-	  if (store.authed) {
-		  msg = msg.replace(/\/[pb]rotip|^brotip: |^protip: |^pro-tip: /i, '');
-		  store.rclient.sadd('protip', msg);
-	  }
-  }
+    if (msg.match(/^\/[pb]rotip:|^brotip:|^protip:|^pro-tip:/i )) {
+	if (store.authed) {
+	    msg = msg.replace(/\/[pb]rotip|^brotip: |^protip: |^pro-tip: /i, '');
+	    store.rclient.rpush('l_protips', msg);
+	}
+    }
 });
