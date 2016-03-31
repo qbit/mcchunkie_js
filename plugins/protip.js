abbb3984675235b60a5dc18985ad27e8043e22c7
Aaron Bieber <aaron@bolddaemon.com>
2016-03-30 19:07:59 -0600

    fixes

--- plugins/protip.js
+++ plugins/protip.js
@@ -18,22 +18,32 @@
 
   if (msg.match(/^\/[pb]rotip\?|^brotip\?|^protip\?|^pro-tip\?/i)) {
       if (store.authed) {
-	  store.rclient.send_command('llrange', ['l_protips', function(e, max) {
+	  store.rclient.send_command('llen', ['l_protips', function(e, max) {
 	      var idx = helper.rand(parseInt(Math.ceil(max), 10));
-	      store.rclient.lindex(idx, 'l_protips', function(e, d) {
+	      store.rclient.lindex('l_protips', idx, function(e, d) {
+		  
+		  if (d) {
+		      d = d.replace(/^\s+//g);
+		  }
+		  
 		  store.rclient.hget('protip_votes', idx, function(e, count) {
-		      d = "(" + count + ") " + d;
-		      cb.call(null, to, from, d, proto);
+		      if (count === null) {
+                          d = "(0) " + d;
+                          cb.call(null, to, from, d, proto);
+                      } else {
+                          d = "(" + count + ") " + d;
+                          cb.call(null, to, from, d, proto);
+                      }
 		  });
 	      });
 	  }]);
       }
-  }
+}
 
-    if (msg.match(/^\/[pb]rotip:|^brotip:|^protip:|^pro-tip:/i )) {
-	if (store.authed) {
-	    msg = msg.replace(/\/[pb]rotip|^brotip: |^protip: |^pro-tip: /i, '');
-	    store.rclient.rpush('l_protips', msg);
-	}
-    }
+if (msg.match(/^\/[pb]rotip:|^brotip:|^protip:|^pro-tip:/i )) {
+if (store.authed) {
+msg = msg.replace(/\/[pb]rotip|^brotip: |^protip: |^pro-tip: /i, '');
+store.rclient.rpush('l_protips', msg);
+}
+}
 });
