2a7b7cede2b214050ae9ef1c5b24b4214eddae21
Aaron Bieber <aaron@bolddaemon.com>
2016-03-31 18:23:51 -0600

    allow getting by index

--- plugins/protip.js
+++ plugins/protip.js
@@ -30,25 +30,38 @@
 	store.rclient.hincrby('protip_votes', id, -1, function() {});
     }
 
+    function getByIDX(idx, fn) {
+	store.rclient.lindex('l_protips', idx, function(e, d) {
+	    
+	    if (d) {
+		d = d.replace(/^\s+/, '');
+	    }
+	    
+	    store.rclient.hget('protip_votes', idx, function(e, count) {
+		if (count === null) {
+		    d = "(0) " + d + " id: " + idx;
+		    fn.call(null, d);
+		} else {
+		    d = "(" + count + ") " + d + " id: " + idx;
+		    fn.call(null, d);
+		}
+	    });
+	});
+    }
+
+    if (msg.match(/^getpt:\d+$/)) {
+	var id = msg.replace('getpt:', '');
+	getByIDX(id, function(s) {
+	    cb.call(null, to, from, s, proto);
+	});
+    }
+
     if (msg.match(/^\/[pb]rotip\?|^brotip\?|^protip\?|^pro-tip\?/i)) {
 	if (store.authed) {
 	    store.rclient.llen('l_protips', function(e, max) {
 		var idx = helper.rand(parseInt(Math.ceil(max), 10));
-		store.rclient.lindex('l_protips', idx, function(e, d) {
-		  
-		    if (d) {
-			d = d.replace(/^\s+/, '');
-		    }
-		  
-		    store.rclient.hget('protip_votes', idx, function(e, count) {
-			if (count === null) {
-			    d = "(0) " + d + " id: " + idx;
-			    cb.call(null, to, from, d, proto);
-			} else {
-			    d = "(" + count + ") " + d + " id: " + idx;
-			    cb.call(null, to, from, d, proto);
-			}
-		    });
+		getByIDX(idx, function(s) {
+		    cb.call(null, to, from, s, proto);
 		});
 	    });
 	}
