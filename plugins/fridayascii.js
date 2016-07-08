87fa6c243d10b057e089c48ec4905636d9373399
ergo_ <johnlam@users.noreply.github.com>
2016-07-08 15:59:52 +0100

    add fridayascii.js
    Not tested. Should align alright. Tgif.

---
+++ plugins/fridayascii.js
@@ -0,0 +1,20 @@
+(function( helper, to, from, msg, store, sh_store, cb, proto ) {
+  'use strict';
+  var resp = "";
+  if ( helper.isRelevant( msg ) ) {
+    if ( msg.match( "/tgif/i" ) ) {
+      resp += "____________ ___________  _____   __\n"
+      resp += "|  ___| ___ \\_   _|  _  \\/ _ \\ \\ / /\n"
+      resp += "| |_  | |_/ / | | | | | / /_\\ \\ V / \n"
+      resp += "|  _| |    /  | | | | | |  _  |\\ /  \n"
+      resp += "| |   | |\\ \\ _| |_| |/ /| | | || |\n"
+      resp += "\\_|   \\_| \\_|\\___/|___/ \\_| |_/\\_/\n"
+
+      setTimeout( function() {
+        resp = '(It really is, somewhere)';
+        cb.call( null, to, from, resp, proto );
+      }, 5000 );
+    }
+  }
+  cb.call( null, to, from, resp, proto );
+});
