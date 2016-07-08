6acd76b9d3483ed891b493218c70319a64fc8131
Aaron Bieber <aaron@bolddaemon.com>
2016-07-08 09:12:55 -0600

    make tg more purdy

--- plugins/fridayascii.js
+++ plugins/fridayascii.js
@@ -3,12 +3,18 @@
   var resp = "";
   if (helper.isRelevant(msg)) {
     if (msg.match(/tgif/i)) {
+      if (proto === "telegram") {
+        resp += "```\n"
+      }
       resp += "____________ ___________  _____   __\n"
       resp += "|  ___| ___ \\_   _|  _  \\/ _ \\ \\ / /\n"
       resp += "| |_  | |_/ / | | | | | / /_\\ \\ V / \n"
       resp += "|  _| |    /  | | | | | |  _  |\\ /  \n"
       resp += "| |   | |\\ \\ _| |_| |/ /| | | || |\n"
       resp += "\\_|   \\_| \\_|\\___/|___/ \\_| |_/\\_/\n"
+      if (proto === "telegram") {
+        resp += "```\n"
+      }
 
       setTimeout(function() {
         resp = '(It really is, somewhere)';
