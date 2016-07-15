8c51bcba02b06ca7de88260d23f5be7a959a9076
Aaron Bieber <aaron@bolddaemon.com>
2016-07-15 08:38:07 -0600

    only ascii on friday!

--- plugins/fridayascii.js
+++ plugins/fridayascii.js
@@ -1,26 +1,20 @@
 (function(helper, to, from, msg, store, sh_store, cb, proto) {
-  'use strict';
-  var resp = "";
-  if (helper.isRelevant(msg)) {
-    if (msg.match(/tgif/i)) {
-      if (proto === "telegram") {
-        resp += "```\n"
-      }
-      resp += "____________ ___________  _____   __\n"
-      resp += "|  ___| ___ \\_   _|  _  \\/ _ \\ \\ / /\n"
-      resp += "| |_  | |_/ / | | | | | / /_\\ \\ V / \n"
-      resp += "|  _| |    /  | | | | | |  _  |\\ /  \n"
-      resp += "| |   | |\\ \\ _| |_| |/ /| | | || |\n"
-      resp += "\\_|   \\_| \\_|\\___/|___/ \\_| |_/\\_/\n"
-      if (proto === "telegram") {
-        resp += "```\n"
-      }
-
-      setTimeout(function() {
-        resp = '(It really is, somewhere)';
-        cb.call(null, to, from, resp, proto);
-      }, 5000);
+    'use strict';
+    var resp = "";
+    if (helper.isRelevant(msg)) {
+	var d = new Date();
+	if (msg.match(/tgif/i)) {
+	    if (d.getDay() === 5) {
+		resp += "____________ ___________  _____   __\n";
+		resp += "|  ___| ___ \\_   _|  _  \\/ _ \\ \\ / /\n";
+		resp += "| |_  | |_/ / | | | | | / /_\\ \\ V / \n";
+		resp += "|  _| |    /  | | | | | |  _  |\\ /  \n";
+		resp += "| |   | |\\ \\ _| |_| |/ /| | | || |\n";
+		resp += "\\_|   \\_| \\_|\\___/|___/ \\_| |_/\\_/\n";
+	    } else {
+		resp = ":'(";
+	    }
+	}
     }
-  }
-  cb.call(null, to, from, resp, proto);
+    cb.call(null, to, from, resp, proto);
 });
