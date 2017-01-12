641913a1f75bdae90745186e094aeb9f03630edf
Aaron Bieber <aaron@bolddaemon.com>
2017-01-11 18:18:39 -0700

    hi

--- plugins/hi.js
+++ plugins/hi.js
@@ -1,12 +1,12 @@
 // Desc: hi
 (function( helper, to, from, msg, store, sh_store, cb, proto ) {
-	'use strict';
-	var resp;
-	if ( helper.isRelevant( msg ) ) {
-		if ( msg.match( /hi/i ) ) {
-			from = from || to;
-			resp = "hi " + from;
-		}
+    'use strict';
+    var resp;
+    if ( helper.isRelevant( msg ) ) {
+	if ( msg.match( /hi$/i ) ) {
+	    from = from || to;
+	    resp = "hi " + from;
 	}
-	cb.call( null, to, from, resp, proto );
+    }
+    cb.call( null, to, from, resp, proto );
 });
